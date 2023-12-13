9be4d05322552eb7176788a4b058580f18df2956
Rodrigo Gastaud <rodrigogastaud_97@hotmail.com>
2023-12-13 00:26:25 -0300

    Modal abrindo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <link rel="stylesheet" href="../../"> -->
-    <link rel="stylesheet"  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" crossorigin="anonymous">
     <title>Document</title>
 </head>
+
 <body>
     <header class="container-fluid">
         <nav class="navbar bg-dark">
             <div class="container-fluid">
                 <a href="">AGENDA TELEFONICA</a>
-                <button class="btn btn-danger">Criar Contato</button>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#criarContato">Criar Contato</button>
             </div>
         </nav>
         <p>teste</p>
@@ -31,5 +33,25 @@
             </thead>
         </table>
     </main>
+    <div class="modal fade" id="criarContato" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5">titulo</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <h1>teste</h1>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Fechar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script> -->
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
